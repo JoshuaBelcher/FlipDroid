796ba364c966faf853dce150836a95f2135a78af
JoshuaBelcher <joshuab48@hotmail.com>
2021-04-15 17:02:12 -0400

    Fixed drawables directory error and layout errors; added Shared Preferences feature; included Night/Day Mode toggle selection in Shared Preferences,

--- app/src/main/java/joshb/android/flipdroid/MainActivity.kt
+++ app/src/main/java/joshb/android/flipdroid/MainActivity.kt
@@ -3,15 +3,36 @@ package joshb.android.flipdroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import androidx.appcompat.app.AppCompatDelegate
+import android.content.SharedPreferences
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Class variables for Loading Preferences & High Scores
+    private val PREF_Name = "savedPrefs"
+    private var nightModeOn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set handle for saved preferences
+        var loadedPref: SharedPreferences = getSharedPreferences(PREF_Name, 0)
+
+        // check loaded preferences for theme data
+        if (loadedPref.contains("nightMode")) {
+            nightModeOn = loadedPref.getBoolean("nightMode", false)
+        }
+
+        // if the user has a saved preference for Night Mode being ON, then it will activated now
+        if (nightModeOn) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
     }
 
     fun playButtonHandler (view: View) {
@@ -26,6 +47,7 @@ class MainActivity : AppCompatActivity() {
 
     fun settingsButtonHandler (view: View) {
         var intent= Intent(this, Settings:: class.java)
+        intent.putExtra("nightModeOn", nightModeOn)
         startActivity(intent)
     }
 
@@ -57,6 +79,7 @@ class MainActivity : AppCompatActivity() {
                 return super.onOptionsItemSelected(item)
             }
             R.id.act_settings->{var intent=Intent(this, Settings:: class.java)
+                intent.putExtra("nightModeOn", nightModeOn)
                 startActivity(intent)
                 return super.onOptionsItemSelected(item)
             }
--- app/src/main/java/joshb/android/flipdroid/Settings.kt
+++ app/src/main/java/joshb/android/flipdroid/Settings.kt
@@ -6,12 +6,57 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import android.content.SharedPreferences
+import android.widget.Switch
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class Settings : AppCompatActivity() {
+    // Class variables for Saving/Loading Preferences & High Scores
+    private val PREF_Name = "savedPrefs"
+    private var editor: SharedPreferences.Editor?=null
+    private var loadedPref: SharedPreferences?=null
+
+    // Class variables for referencing widgets
+    private var themeSwitch: Switch? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        // get reference to widgets
+        themeSwitch = findViewById<Switch>(R.id.switch_theme)
+
+        // get Night Mode status passed from MainActivity
+        var intent = getIntent()
+        var nightModeOn = intent.getBooleanExtra("nightModeOn", false)
+
+        // toggle theme switch on if Night Mode status was previously saved as activated
+        // NOTE: this ensures consistent switch operation regardless of previously saved status, i.e.,
+        // if Night Mode was turned on by loaded preferences, the switch will not sit erroneously in
+        // the "Off" position
+        if (nightModeOn) {
+            themeSwitch?.isChecked = true
+        }
+
+        // set reference for saved preferences
+        loadedPref = getSharedPreferences(PREF_Name, 0)
+
+        // set handle for preferences editor and assign preferences to be edited
+        editor = (loadedPref as SharedPreferences).edit()
+
+    }
+
+    fun themeToggleHandler (view: View) {
+        if(themeSwitch?.isChecked!!){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            editor?.putBoolean("nightMode", true)
+            editor?.commit()
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            editor?.putBoolean("nightMode", false)
+            editor?.commit()
+        }
     }
 
     fun backButtonHandler (view: View) {
